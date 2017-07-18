24bd2e5d75918e5b39c3831ef246069c23d131a3
Fare9 <farenain9@gmail.com>
2017-07-18 11:23:04 +0200

    Added dex2jar analysis

---
+++ Tool2/Core/dex2jarAnalysis.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+'''
+    Author: Fare9 <farenain9(at)gmail(dot)com>
+    Program: AndroidSwissKnife
+    Folder: Core
+    File: dex2jarAnalysis.py
+'''
+
+'''
+    Use dex2jar to decompiling code
+'''
+
+import os
+from Utilities.Print_functions import __print_verbosity
+
+def getjarFunc(file):
+    '''
+    Function to call dex2jar, then you can see the code with others tools
+
+    :param str file: apk file to analyze
+    :return: None
+    :rtype: None
+    '''
+
+    __print_verbosity(1,"[+] Creating Directory and jar from apk...")
+    actualDirectory = os.getcwd()  # get actual directory
+
+    # First take a look if path is relative or absolute
+    isRelative = os.path.isabs(file)
+
+    if isRelative:
+        # if relative, well get absolute path
+        file = os.path.abspath(file)
+
+    nameNoAPK = file.replace('.apk', '')  # name without .apk
+    nameDEX2JAR = nameNoAPK + '_dex2jar.jar'  # name from dex2jar output
+
+    __print_verbosity(1,"[+] Creating file %s" % nameDEX2JAR)
+    sentence = 'd2j-dex2jar ' + file
+    os.system(sentence)
+
+    __print_verbosity(1,"[+] Creating folder %s_CLASS and change directory" % nameNoAPK)
+    print()
+    os.mkdir('%s_CLASS' % nameNoAPK)
+    os.chdir('%s_CLASS' % nameNoAPK)
+
+    print("[+] Creating classes files")
+    sentence = 'unzip ../' + nameDEX2JAR
+    os.system(sentence)
+
+    print("[+] Returning to: " + actualDirectory)
+    os.chdir(actualDirectory)
